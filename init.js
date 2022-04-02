af5080dd94b9624c1ba00db0892cfacf7dacc7fb
jhhg04 <jhhg04@gmail.com>
2022-04-01 20:59:05 -0500

    add

--- init.js
+++ init.js
@@ -1,6 +1,6 @@
 const config = {
-  width: 320,
-  height: 180,
+  width: 320 * 2,
+  height: 180 * 2,
   parent: 'container',
   type: Phaser.AUTO,
   scene: {
@@ -8,16 +8,36 @@ const config = {
     create: create,
     update: update,
   },
+  physics: {
+    default: 'arcade',
+    arcade: {
+      gravity: {
+        y: 500,
+      },
+    },
+  },
 };
 
 var game = new Phaser.Game(config);
 
 function preload() {
-  console.log('soy preload');
+  this.load.image('bird', './assets/bird.png');
+  // this.load.image('bird', './assets/bird_dos.png');
 }
 function create() {
-  console.log('soy create');
+  this.bird = this.physics.add.image(80, 100, 'bird');
+  this.bird.setScale(2);
+  this.bird.flipX = false;
+  this.bird.setOrigin(0.5);
+  // this.bird.setAngle(30);
+  // Physics
+  this.bird.setCollideWorldBounds(true);
+  this.bird.setBounce(0.3);
+  // this.bird.setAcceleration(50, 0);
+  this.bird.setVelocity(50, 0);
 }
 function update(time, delta) {
-  console.log(delta);
+  // this.bird.angle++;
+  // this.bird.x++;
+  // this.bird.y++;
 }
